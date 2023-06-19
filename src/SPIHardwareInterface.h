e77415e5d325f9e52708996cefa6cf5b5d676bcd
testusuke <silverbro007@icloud.com>
2023-06-19 22:40:08 +0900

    feat: create abstruct spi interface class

---
+++ src/SPIHardwareInterface.h
@@ -0,0 +1,65 @@
+#ifndef _SPI_HARDWARE_INTERFACE_H_
+#define _SPI_HARDWARE_INTERFACE_H_
+
+class SPIHardwareInterface
+{
+public:
+  
+  /////////////////////////////////
+  //  Chip select
+  /////////////////////////////////
+
+  /**
+   * @brief Configure spi settings and begin transaction
+   * 
+   * @param configuration_mode If this is true, configure spi as configuration mode
+   */
+  virtual void beginTransaction(bool configuration_mode = false);
+
+  /**
+   * @brief Finish transaction
+   * 
+   */
+  virtual void endTransaction();
+
+  /**
+   * @brief Transfer data specified length
+   * 
+   * @param buffer The data buffer which will transfer
+   * @param length The data length
+   * @return int The number of bytes written and read from the device
+   */
+  virtual int transfer(const uint8_t *buffer, int length) = 0;
+  
+  /**
+   * @brief Transfer 16-bit data
+   * 
+   * @param data The data to transfer
+   * @return int The number of bytes written and read from the device
+   */
+  virtual int transfer16(const uint16_t data) = 0;
+
+  /////////////////////////////////
+  //  Chip select
+  /////////////////////////////////
+
+  /**
+   * @brief initialize CS pin
+   * 
+   */
+  virtual void initCS();
+
+  /**
+   * @brief assert cs
+   * 
+   */
+  virtual inline void assertCS();
+
+  /**
+   * @brief assert cs
+   * 
+   */
+  virtual inline void deassertCS();
+};
+
+#endif
