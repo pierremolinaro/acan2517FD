0aa65127b4cc1dac96892cb82798e9210024b2e2
testusuke <silverbro007@icloud.com>
2023-06-20 13:02:32 +0900

    feat: Mbed support

---
+++ src/MbedHardwareSPI.cpp
@@ -0,0 +1,75 @@
+#if defined(__arm__) && defined(MBED_SB)
+
+#include "MbedHardwareSPI.h"
+
+MbedHardwareSPI:MbedHardwareSPISPIClass & dev, const uint8_t cs)
+  : _dev(dev), _cs(cs)
+{
+    //  init spi settings
+    //  configuration mode
+    _configuration_mode = true;
+    //  frequency
+    _dev.frequency(1000UL * 1000);
+    //  8bits mode_0
+    _dev.format(8, 0);
+}
+
+void MbedHardwareSPI:beginTransaction(bool configuration_mode)
+{
+    if(_configuration_mode != configuration_mode) {
+        if(configuration_mode) {
+            //  frequency
+            _dev.frequency(1000UL * 1000)
+        }
+        else {
+            //  frequency
+            _dev.frequency(_spiClock)
+        }
+
+        _configuration_mode = configuration_mode;
+    }
+
+    //  TODO: lock?
+}
+
+void MbedHardwareSPI:endTransaction()
+{
+    //  nope
+    //  TODO: unlock?
+}
+
+int MbedHardwareSPI:transfer(const uint8_t *buffer, int length)
+{
+    _dev.write(buffer, length, buffer, length);
+
+    return 0;
+}
+
+int MbedHardwareSPI:transfer16(const uint16_t data)
+{
+    _dev.write(data);
+    return 0;
+}
+
+void MbedHardwareSPI:initCS()
+{
+    //  nop
+}
+
+inline void MbedHardwareSPI:assertCS()
+{
+    _cs.write(0);
+}
+
+inline void MbedHardwareSPI:deassertCS()
+{
+    _cs.write(1);
+}
+
+void MbedHardwareSPI:setSPIClock(const uint32_t spiClock)
+{
+    //  super
+    SPIHardwareInterface::setSPIClock(spiClock);
+}
+
+#endif
---
+++ src/MbedHardwareSPI.h
@@ -0,0 +1,42 @@
+#if defined(__arm__) && defined(MBED_SB)
+
+#ifndef _MBED_HARDWARE_SPI_H_
+#define _MBED_HARDWARE_SPI_H_
+
+#include "mbed.h"
+
+#include "SPIHardwareInterface.h"
+
+class MbedHardwareSPI : public SPIHardwareInterface
+{
+public:
+    MbedHardwareSPI(SPI & dev, const PinName csPin);
+
+    virtual void beginTransaction(bool configuration_mode = false);
+
+    virtual void endTransaction();
+
+    virtual int transfer(const uint8_t *buffer, int length);
+
+    virtual int transfer16(const uint16_t data);
+
+    virtual void initCS();
+
+    virtual inline void assertCS();
+
+    virtual inline void deassertCS();
+
+    void setSPIClock(const uint32_t spiClock) override;
+
+private:
+    //  SPI
+    SPI & _dev;
+    //  CS
+    DigitalOut _cs;
+
+    //  mode
+    bool _configuration_mode;
+};
+#endif
+
+#endif
